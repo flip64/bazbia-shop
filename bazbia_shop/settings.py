3e953a71c709cd085db0d5e2c4310a18c81670e7
flip <jr64.naderloo@gmail.com>
2025-07-12 00:48:30 +0330

    edit

--- bazbia_shop/settings.py
+++ bazbia_shop/settings.py
@@ -16,6 +16,15 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
