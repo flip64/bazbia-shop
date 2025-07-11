6bbb3d02782184d79a87439152035dd83cd5955f
flip <jr64.naderloo@gmail.com>
2025-07-11 21:36:59 +0330

    creat template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category, Tag
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(is_active=True)
+
+    # فیلتر با دسته یا تگ
+    category_slug = request.GET.get('category')
+    tag_slug = request.GET.get('tag')
+
+    if category_slug:
+        products = products.filter(category__slug=category_slug)
+    if tag_slug:
+        products = products.filter(tags__slug=tag_slug)
+
+    context = {
+        'products': products
+    }
+    return render(request, 'products/product_list.html', context)
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    variants = product.variants.all()
+    specifications = product.specifications.all()
+    images = product.images.all()
+    videos = product.videos.all()
+
+    context = {
+        'product': product,
+        'variants': variants,
+        'specifications': specifications,
+        'images': images,
+        'videos': videos
+    }
+    return render(request, 'products/product_detail.html', context)
+
