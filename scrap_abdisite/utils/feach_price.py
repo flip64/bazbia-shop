8cc6bbe39b414b52ee27b9c9ad1070b6e78dcacb
flip <jr64.naderloo@gmail.com>
2025-07-14 16:59:34 +0330

    edit feach price

--- scrap_abdisite/utils/feach_price.py
+++ scrap_abdisite/utils/feach_price.py
@@ -66,6 +66,7 @@ def send_email_view(message):
 
 
 def fetch_product_details(url):
+    print(f"Fetching URL: {url}")
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                       "(KHTML, like Gecko) Chrome/115.0 Safari/537.36"
