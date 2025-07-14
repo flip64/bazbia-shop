b890916854cdd66fa84e2fd6e4830132a67bbf28
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-14 12:51:29 +0330

    Update settings.py
    Edit sitting for send email

--- bazbia_shop/settings.py
+++ bazbia_shop/settings.py
@@ -170,3 +170,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# تنظیمات SMTP برای Gmail
+# تنظیمات ایمیل
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'  # تبدیل به boolean
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
