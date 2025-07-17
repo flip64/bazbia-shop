33d740f97b8ebde957ddb56745495a8932bb72c5
flip <jr64.naderloo@gmail.com>
2025-07-17 15:58:08 +0330

    edit url for fix erorre

--- bazbia_shop/urls.py
+++ bazbia_shop/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls', namespace='products')),
-    path('', include('products.urls', namespace='products')),
+ #   path('', include('products.urls', namespace='products')),
     path('scrap_abdisite/', include('scrap_abdisite.urls', namespace='scrap_abdisite')),
     path('schema/', include('schema_viewer.urls')),
 
