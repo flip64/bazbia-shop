f3298eecb5725c1bd5705379abb7f01133792a6e
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-21 10:01:39 +0330

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('home', views.home, name='home'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
-    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
+  #  path('category/<slug:slug>/', views.category_detail, name='category_detail'),
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/new/', views.category_create, name='category_create'),
+    path('categories/edit/<int:pk>/', views.category_edit, name='category_edit'),
+]
 
+
+
+
+
+urlpatterns = [
+    
 ]
