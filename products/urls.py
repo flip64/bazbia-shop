5dd338e74ff52149f97a7f22e9c206649e56873e
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-21 10:12:04 +0330

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
+    path('1', views.product_list, name='product_list'),
     path('home', views.home, name='home'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
   #  path('category/<slug:slug>/', views.category_detail, name='category_detail'),
