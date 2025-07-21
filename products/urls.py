3075ec0ba9df6d67914a6c96e13bae10386e4107
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-21 17:39:46 +0330

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,6 @@ app_name = 'products'  # برای namespace مهمه
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),  # /products/
-    path('<slug:slug>/', views.product_detail, name='product_detail'),  # /products/some-product/
     path('category/', views.category_detail, name='category_detail'),  # /products/category/slug/
     path('categories/', views.category_list, name='category_list'),  # /products/categories/
     path('category/create/', views.category_create, name='category_create'),  # /products/category/create/
