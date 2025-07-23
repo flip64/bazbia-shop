ad9f6a06ed293b661b76250da57200ee0adf1f62
flip64 <jr64.naderloo@gmail.com>
2025-07-23 15:54:36 +0330

    freast commit if mobile

--- products/API/category_api.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-from products.models import Category
-
-def list_categories(request):
-    categories = Category.objects.all().values('id', 'name', 'slug')
-    data = list(categories)  # تبدیل QuerySet به لیست ساده
-    return JsonResponse(data, safe=False)
--- products/API/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import category_api
-
-urlpatterns = [
-    path('categories/', category_api.list_categories, name='list_categories'),
-]
