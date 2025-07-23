cd506c5dccde7a741822f305dbb63b1ef8fd8cf9
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-23 18:39:53 +0330

    Update category_api.py

--- products/api/category_api.py
+++ products/api/category_api.py
@@ -2,6 +2,20 @@ from django.http import JsonResponse
 from products.models import Category
 
 def list_categories(request):
-    categories = Category.objects.all().values('id', 'name', 'slug')
-    data = list(categories)  # تبدیل QuerySet به لیست ساده
+    def build_tree(parent=None):
+        categories = Category.objects.filter(parent=parent).values('id', 'name', 'slug')
+        tree = []
+        for cat in categories:
+            children = build_tree(parent=cat['id'])
+            item = {
+                'id': cat['id'],
+                'name': cat['name'],
+                'slug': cat['slug'],
+            }
+            if children:
+                item['children'] = children
+            tree.append(item)
+        return tree
+
+    data = build_tree()
     return JsonResponse(data, safe=False)
