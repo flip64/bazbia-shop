844d179c66a1bc4b182d41195efc172c4572120c
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-23 08:18:32 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ app_name = 'core'
 
 urlpatterns = [
     path("", views.home, name="root"),
-    path("/home", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
