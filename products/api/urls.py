b74bef22dff7c081fac70b64d23119fd21f94cec
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-23 18:10:03 +0330

    Update urls.py

--- products/api/urls.py
+++ products/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import category_api
 
 urlpatterns = [
-    path('categories/', category_api.list_categories, name='list_categories'),
+    path('categories/', category_api.list_categories, name='list_categories_api'),
 ]
