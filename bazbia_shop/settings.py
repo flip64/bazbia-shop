3b1cdf5c9e772a31aa3e1b9708a7c1b4c82149b8
flip64 <65002468+flip64@users.noreply.github.com>
2025-07-29 12:55:05 +0330

    Update settings.py

--- bazbia_shop/settings.py
+++ bazbia_shop/settings.py
@@ -208,3 +208,6 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'  # تبدیل به boolean
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+
+CART_SESSION_ID = 'cart'
