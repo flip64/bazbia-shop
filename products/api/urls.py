69ece815a690c6b00e1e6b876e3ac29b2326ac00
flip64 <65002468+flip64@users.noreply.github.com>
2025-08-27 10:02:03 +0330

    Update urls.py

--- products/api/urls.py
+++ products/api/urls.py
@@ -8,10 +8,11 @@ from products.api.views import NewProductsAPIView
 urlpatterns = [
     path('', views.ProductListAPIView.as_view(), name='product-list'),
     path('<slug:slug>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
-    path('categories/', views.ProductListCategoryAPIView.as_view, name='list_categories_api'),
+    path('categories/', views.CategoryListAPIView.as_view(), name='category-list'),
+    path('categories/<slug:slug>/', views.ProductListCategoryAPIView.as_view, name='list_categories_api'), #   محصولات یک دسته خاص را برمی گرداند    
     path('import-categories/', category_api.import_categories, name='import-categories'),
-    path('products/categories/', views.CategoryListAPIView.as_view(), name='category-list'),
-    path('products/categories/<slug:slug>', views.ProductListCategoryAPIView.as_view(), name='category-list-products'),      #   محصولات یک دسته خاص را برمی گرداند  
+    
+         
     path('specialproduct/', views.SpecialProductListAPIView.as_view(), name='specialproduct'),
     path('new_products/', NewProductsAPIView.as_view(), name='newproducts'),
 
