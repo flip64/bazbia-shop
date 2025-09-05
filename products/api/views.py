207f884ce9fb7134f0b882c0d8a27ea455dec469
flip64 <65002468+flip64@users.noreply.github.com>
2025-09-05 07:05:12 +0330

    Update views.py

--- products/api/views.py
+++ products/api/views.py
@@ -12,7 +12,6 @@ from products.api.serializers import (
     ProductSerializer,
     ProductDetailSerializer,
     SpecialProductSerializer,
-    NewProductSerializer,
     CategorySerializer,
     ProductListSerializer
 )
