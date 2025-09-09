59aea7f12ddba6151c5948922f9c74dde8e930f9
flip64 <65002468+flip64@users.noreply.github.com>
2025-09-09 19:15:35 +0330

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from .views import WeeklyBestSellersAPIView
 
 urlpatterns = [
-    path("weeklyBestSellers/", WeeklyBestSellersAPIView.as_view(), name="weekly-best-sellers"),
 ]
