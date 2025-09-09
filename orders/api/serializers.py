c321281c288677d37284c43cc542166a79806d5c
flip64 <65002468+flip64@users.noreply.github.com>
2025-09-09 19:42:48 +0330

    Update serializers.py

--- orders/api/serializers.py
+++ orders/api/serializers.py
@@ -5,7 +5,12 @@ from products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(read_only=True)
     thumb = serializers.ImageField(source='thumb', read_only=True)
+    created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'slug', 'base_price', 'category', 'thumb', 'created_at']
+
+    def get_created_at(self, obj):
+        # تبدیل تاریخ به رشته ISO بدون ارور tzinfo
+        return obj.created_at.isoformat()
