824704f16a68d65b08debff083465017062454c7
flip64 <65002468+flip64@users.noreply.github.com>
2025-10-09 00:42:01 +0330

    Update models.py

--- products/models.py
+++ products/models.py
@@ -147,7 +147,7 @@ class ProductVariant(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.product.name} - {self.sku}"
+        return f"{self.product.name}"
 
 
 
