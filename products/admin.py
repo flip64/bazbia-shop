9564419afd64f4d8e9ed5dd3c3278614c37a8fc4
flip64 <65002468+flip64@users.noreply.github.com>
2025-10-09 01:13:01 +0330

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import Product,ProductImage,ProductSpecification,ProductVariant,ProductVideo 
-from .models import  Category,SpecialProduct
+from .models import  Category,SpecialProduct,AttributeValue,Attribute
 
 
 
@@ -12,7 +12,8 @@ admin.site.register(ProductVariant)
 admin.site.register(ProductVideo)
 admin.site.register(SpecialProduct)
 admin.site.register(Category)
-
+admin.site.register(Attribute)
+admin.site.register(AttributeValue)
 
 
 
