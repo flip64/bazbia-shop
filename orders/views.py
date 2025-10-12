6ce6171536eb4e382bfe3942cf8801179aaf42e3
flip64 <65002468+flip64@users.noreply.github.com>
2025-10-12 09:54:41 +0330

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from .models import Cart
 
-# Create your views here.
+User = get_user_model()
+
+def view_user_cart(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    
+    # سعی می‌کنیم آخرین سبد خرید کاربر را بگیریم
+    cart = Cart.objects.filter(user=user).order_by('-updated_at').first()
+    
+    if cart:
+        cart_items = cart.items.all()
+        total = sum(item.total_price() for item in cart_items)
+    else:
+        cart_items = []
+        total = 0
+
+    context = {
+        'user': user,
+        'cart': cart,
+        'cart_items': cart_items,
+        'total': total
+    }
+    return render(request, 'view_cart.html', context)
