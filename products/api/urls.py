759c0d18ca97e393c3f803a4c29a0bbf17bd5e77
flip64 <65002468+flip64@users.noreply.github.com>
2025-10-20 15:44:26 +0330

    Update urls.py

--- products/api/urls.py
+++ products/api/urls.py
@@ -2,18 +2,22 @@ from django.urls import path
 from . import category_api
 from products.api import views
 
-
 urlpatterns = [
+    # لیست محصولات ساده با فیلتر دسته
     path('', views.ProductListAPIView.as_view(), name='product-list'),
 
-    # مسیرهای خاص باید قبل از slug باشند
+    # لیست کامل محصولات با جزئیات
+    path('full-products/', views.FullProductListAPIView.as_view(), name='full-product-list'),
+
+    # مسیرهای دسته‌بندی
     path('categories/', views.CategoryListAPIView.as_view(), name='category-list'),
     path('categories/<slug:slug>/', views.ProductListCategoryAPIView.as_view(), name='list_categories_api'),
     path('import-categories/', category_api.import_categories, name='import-categories'),
+
+    # محصولات ویژه و جدید
     path('specialproduct/', views.SpecialProductListAPIView.as_view(), name='specialproduct'),
     path('new_products/', views.NewProductsAPIView.as_view(), name='newproducts'),
-    
-    
-    # در آخر مسیر slug رو بذار
+
+    # جزئیات محصول بر اساس slug
     path('<slug:slug>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
 ]
