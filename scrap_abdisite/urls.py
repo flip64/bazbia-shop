9780cbf00b448b0347dc9d3865406f06254f1979
flip64 <65002468+flip64@users.noreply.github.com>
2025-10-22 22:44:37 +0330

    Update urls.py

--- scrap_abdisite/urls.py
+++ scrap_abdisite/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from scrap_abdisite.views.price_history_view import price_history_view
+from .views.price_history_views import price_history_view
 
 app_name = "scrap_abdisite"
 
