54d13b95184e4f8a31150e4ecfde481b5ed5ec5d
flip64 <65002468+flip64@users.noreply.github.com>
2025-11-08 17:46:40 +0330

    Update urls.py

--- scrap_abdisite/api/urls.py
+++ scrap_abdisite/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('watched-urls/all/', views.watched_urls_all_api, name='watched_urls_all_api'),
   # API برای بروزرسانی قیمت یک WatchedURL مشخص
-    path('api/watched-url/<int:pk>/update-price/', views.UpdateWatchedURLPriceAPIView.as_view(), name='update_watched_url_price_api'),
+    path('watched-url/<int:pk>/update-price/', views.UpdateWatchedURLPriceAPIView.as_view(), name='update_watched_url_price_api'),
     
 ]
