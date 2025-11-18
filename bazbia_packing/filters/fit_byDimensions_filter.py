ddc91797d4403b456252b80e21cc70cef0b602b6
flip <jr64.naderloo@gmail.com>
2025-11-18 06:58:01 +0330

    edit packing

--- bazbia_packing/filters/fit_byDimensions_filter.py
+++ bazbia_packing/filters/fit_byDimensions_filter.py
@@ -1,27 +1,24 @@
 import itertools
 
 class FitByDimensionsFilter:
-    def max_items_in_box(self, box_dims, item_dims):
+    def can_fit_item_in_box(self, box_dims, item_dims):
         """
-        محاسبه بیشترین تعداد آیتمی که در جعبه جا می‌شود (با چرخش)
+        محاسبه ابنکه ایتم در جعبه جا مبسود با نه 
         """
+        box_dims = sorted(box_dims)
+        item_dims = sorted(item_dims)
+
+
         L_box, W_box, H_box = box_dims
         l_item, w_item, h_item = item_dims
-
-        rotations = set(itertools.permutations([l_item, w_item, h_item]))
-        max_count = 0
-
-        for rot in rotations:
-            l, w, h = rot
-            nx = L_box // l
-            ny = W_box // w
-            nz = H_box // h
-            count = nx * ny * nz
-            max_count = max(max_count, count)
-
-        return max_count
+        return (
+            L_box >= l_item and
+            W_box >= w_item and
+            H_box >= h_item
+         )
 
     def filter(self, boxes, items):
+
         """
         بررسی آیتم‌ها دونه‌دونه و حذف جعبه‌هایی که آیتم در آن‌ها جا نمی‌شود.
         """
@@ -30,24 +27,26 @@ class FitByDimensionsFilter:
 
         # مرتب‌سازی جعبه‌ها از کوچک به بزرگ
         boxes_sorted = sorted(boxes, key=lambda b: b['length'] * b['width'] * b['height'])
+        
         remaining_boxes = boxes_sorted.copy()
+        new_remainig = []
 
         for item in items:
             item_dims = (item['length'], item['width'], item['height'])
-            new_remaining = []
-
             for box in remaining_boxes:
-                box_dims = (box['length'], box['width'], box['height'])
-                if self.max_items_in_box(box_dims, item_dims) >= 1:
-                    # آیتم در این جعبه جا شد → جعبه را نگه دار
-                    new_remaining.append(box)
+              print("box" , box)
+              box_dims = (box['length'], box['width'], box['height'])
+              print(self.can_fit_item_in_box(box_dims, item_dims))
+              if not self.can_fit_item_in_box(box_dims, item_dims):
                 # اگر جا نشد → جعبه حذف می‌شود
+                break  
+            # آیتم در این جعبه جا شد → جعبه را نگه دار
+            new_remainig.append(box)
 
-            remaining_boxes = new_remaining
 
-            if not remaining_boxes:
+            if not new_remainig:
                 # اگر هیچ جعبه‌ای برای آیتم‌ها باقی نماند → خروجی خالی
                 return []
 
         # جعبه‌هایی که برای همه آیتم‌ها مناسب هستند
-        return remaining_boxes
+        return new_remainig
