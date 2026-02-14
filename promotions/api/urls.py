9d48e77c9b44b96346193e3cda820cf567ab2d4d
flip64 <65002468+flip64@users.noreply.github.com>
2026-02-14 17:53:28 +0330

    Update urls.py

--- promotions/api/urls.py
+++ promotions/api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from promotions.views import BannerViewSet
+from promotions.api.views import BannerViewSet
 
 router = DefaultRouter()
 router.register(r'banners', BannerViewSet)
