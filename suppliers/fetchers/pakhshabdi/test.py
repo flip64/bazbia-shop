d41f6758a8436b343a2d8da4a2c4a4608891f272
flip64 <65002468+flip64@users.noreply.github.com>
2026-06-30 09:49:39 +0330

    Update test.py

--- suppliers/fetchers/pakhshabdi/test.py
+++ suppliers/fetchers/pakhshabdi/test.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from utils import check_product
 
-url = raw_input("Product URL: ").strip()
+url = "https://pakhshabdi.com/product/%da%86%d8%b3%d8%a8-%d8%a2%d9%84%d9%88%d9%85%db%8c%d9%86%db%8c%d9%88%d9%85%db%8c-%d8%af%d9%88%d8%b1-%da%af%d8%a7%d8%b2-5-%d8%b3%d8%a7%d9%86%d8%aa/"
 
 result = check_product(url)
 
