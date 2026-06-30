3113f0b38994960466a9a03cc7e5cafb974a6f64
flip64 <65002468+flip64@users.noreply.github.com>
2026-06-30 07:51:00 +0330

    Update fetch_product_list.py

--- suppliers/fetchers/pakhshabdi/fetch_product_list.py
+++ suppliers/fetchers/pakhshabdi/fetch_product_list.py
@@ -7,7 +7,7 @@ import re
 import requests
 import xml.etree.ElementTree as ET
 from multiprocessing.dummy import Pool as ThreadPool
-
+from utils import check_product
 BASE_URL = "https://pakhshabdi.com/sitemap_index.xml"
 
 HEADERS = {
@@ -20,47 +20,6 @@ NAMESPACE = {
 
 session = requests.Session()
 session.headers.update(HEADERS)
-
-
-def check_product(url):
-    try:
-        r = session.get(url, timeout=20)
-
-        if r.status_code != 200:
-            return None
-
-        html = r.text
-
-        name = re.search(
-            r'<h1[^>]*class="[^"]*product_title[^"]*"[^>]*>(.*?)</h1>',
-            html,
-            re.I | re.S
-        )
-
-        stock = re.search(
-            r'class="stock\s+in-stock"[^>]*>\s*(\d+)',
-            html,
-            re.I
-        )
-
-        if not stock:
-            return None
-
-        price = re.search(
-            r'property="product:price:amount"\s+content="(\d+)"',
-            html,
-            re.I
-        )
-
-        return {
-            "name": re.sub(r"<.*?>", "", name.group(1)).strip() if name else "",
-            "price": int(price.group(1)) if price else 0,
-            "stock": int(stock.group(1)),
-            "url": url
-        }
-
-    except:
-        return None
 
 
 def get_product_sitemaps():
