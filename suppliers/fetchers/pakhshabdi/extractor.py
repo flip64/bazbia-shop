84f091462a5f7f0c7d1ca8f897dd42c91faf8233
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-01 15:47:43 +0330

    Create extractor.py

---
+++ suppliers/fetchers/pakhshabdi/extractor.py
@@ -0,0 +1,27 @@
+from products.services.product_data import ProductData
+
+from .fetcher import (
+    fetch_product_details,
+    extract_quantity,
+    extract_product_images,
+    extract_specifications,
+    extract_tags,
+)
+
+
+def extract_product_data(url):
+    data = ProductData()
+
+    name, price = fetch_product_details(url)
+
+    data.name = name
+    data.price = price or 0
+    data.quantity = extract_quantity(url) or 0
+
+    data.images = extract_product_images(url)
+    data.specifications = extract_specifications(url)
+    data.tags = extract_tags(url)
+
+    data.supplier_url = url
+
+    return data
