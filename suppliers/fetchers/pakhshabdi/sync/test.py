9426c6f7212c0fc1f79eb9c51fc1828e4046d47f
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-01 19:06:13 +0330

    Update test.py

--- suppliers/fetchers/pakhshabdi/sync/test.py
+++ suppliers/fetchers/pakhshabdi/sync/test.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+BASE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../../../")
+)
+sys.path.insert(0, BASE_DIR)
+
+print(BASE_DIR)
+
+
 from extractor  import extract_product_data
 url = "https://pakhshabdi.com/product/%da%86%d8%b3%d8%a8-%d8%a2%d9%84%d9%88%d9%85%db%8c%d9%86%db%8c%d9%88%d9%85%db%8c-%d8%af%d9%88%d8%b1-%da%af%d8%a7%d8%b2-5-%d8%b3%d8%a7%d9%86%d8%aa/"
 print (extract_product_data.name)
