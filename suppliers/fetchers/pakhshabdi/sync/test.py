6a94ec1de3ee0cbdd0e11a85dae2783b4e24f003
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-01 19:14:10 +0330

    Update test.py

--- suppliers/fetchers/pakhshabdi/sync/test.py
+++ suppliers/fetchers/pakhshabdi/sync/test.py
@@ -11,4 +11,6 @@ print(BASE_DIR)
 
 from extractor  import extract_product_data
 url = "https://pakhshabdi.com/product/%da%86%d8%b3%d8%a8-%d8%a2%d9%84%d9%88%d9%85%db%8c%d9%86%db%8c%d9%88%d9%85%db%8c-%d8%af%d9%88%d8%b1-%da%af%d8%a7%d8%b2-5-%d8%b3%d8%a7%d9%86%d8%aa/"
-print (extract_product_data.name)
+product = extract_product_data(url)
+
+print (product.name)
