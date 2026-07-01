2be2ca2f44401da23dee10f4d8fff833edd74721
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-02 01:51:29 +0330

    Update product_creator.py

--- products/services/product_creator.py
+++ products/services/product_creator.py
@@ -1,1 +1,58 @@
+# -*- coding: utf-8 -*-
 
+"""
+سرویس‌های ایجاد اطلاعات محصول
+
+تمام عملیات ساخت محصول از طریق این فایل انجام می‌شود.
+"""
+
+from products.models import (
+    Product,
+    ProductVariant,
+    ProductSpecification,
+    ProductImage,
+    ProductVideo,
+    Tag,
+)
+
+
+def create_product(product_data):
+    """
+    ایجاد محصول
+    """
+    raise NotImplementedError
+
+
+def create_variant(product, product_data):
+    """
+    ایجاد واریانت محصول
+    """
+    raise NotImplementedError
+
+
+def create_specifications(product, product_data):
+    """
+    ایجاد مشخصات محصول
+    """
+    raise NotImplementedError
+
+
+def create_tags(product, product_data):
+    """
+    ایجاد تگ‌های محصول
+    """
+    raise NotImplementedError
+
+
+def create_images(product, product_data):
+    """
+    ایجاد تصاویر محصول
+    """
+    raise NotImplementedError
+
+
+def create_videos(product, product_data):
+    """
+    ایجاد ویدئوهای محصول
+    """
+    raise NotImplementedError
