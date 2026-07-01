643b49dc23f3f6baf794d75e512ed7f820e1a31a
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-01 07:57:33 +0330

    Create logger.py

---
+++ suppliers/fetchers/pakhshabdi/sync/logger.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+from datetime import datetime
+
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+LOG_DIR = os.path.join(BASE_DIR, "scrap_abdisite", "logs")
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+
+LOG_FILE = os.path.join(
+    LOG_DIR,
+    "supplier_sync_{:%Y%m%d_%H%M%S}.log".format(datetime.now())
+)
+
+
+logger = logging.getLogger("supplier_sync")
+logger.setLevel(logging.INFO)
+
+
+if not logger.handlers:
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s",
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    file_handler = logging.FileHandler(
+        LOG_FILE,
+        encoding="utf-8"
+    )
+
+    console_handler = logging.StreamHandler()
+
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+
+def info(message):
+    logger.info(message)
+
+
+def warning(message):
+    logger.warning(message)
+
+
+def error(message):
+    logger.error(message)
+
+
+def exception(message):
+    logger.exception(message)
