7930ccc876751a80fe2156c425ff1217a90b55cd
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-02 01:43:29 +0330

    Create get_supplir_abdi.py

---
+++ suppliers/fetchers/pakhshabdi/get_supplir_abdi.py
@@ -0,0 +1,20 @@
+from suppliers.data.supplier_data import SupplierData
+from suppliers.services.get_supplier import get_supplier
+from suppliers.services.create_supplier import create_supplier
+
+
+def get_or_create_supplier():
+    supplier = get_supplier("پخش عبدی")
+
+    if supplier is None:
+        data = SupplierData()
+        data.name = "پخش عبدی"
+        data.website = "https://pakhshabdi.com"
+        data.phone = ""
+        data.email = ""
+        data.address = ""
+        data.note = "تأمین‌کننده اصلی بازبیا"
+
+        supplier = create_supplier(data)
+
+    return supplier
