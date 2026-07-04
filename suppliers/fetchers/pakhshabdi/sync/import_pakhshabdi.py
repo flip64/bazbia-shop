cf2e3e7297771be733862f1ca2bf69a824f0546a
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-04 19:43:59 +0330

    Update import_pakhshabdi.py

--- suppliers/fetchers/pakhshabdi/sync/import_pakhshabdi.py
+++ suppliers/fetchers/pakhshabdi/sync/import_pakhshabdi.py
@@ -1,29 +1,30 @@
 import os
+import sys
 import django
+
+# ریشه پروژه
+BASE_DIR = "/home/bazbiair/bazbia"
+sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bazbia_shop.settings")
 django.setup()
 
-
-
-from suppliers.fetchers.pakhshabdi.sync.json_loader  import load_available_products 
-from suppliers.fetchers.pakhshabdi.sync.find_offer import find_offer_by_url
-from suppliers.fetchers.pakhshabdi.sync.json_loader import load_available_products 
+from suppliers.fetchers.pakhshabdi.sync.json_loader import load_available_products
 from suppliers.fetchers.pakhshabdi.sync.find_offer import find_offer_by_url
 from suppliers.fetchers.pakhshabdi.sync.create_product_from_url import create_product_from_url
 from suppliers.fetchers.pakhshabdi.sync.updater import update_offer
-from suppliers.fetchers.pakhshabdi.get_supplier_abdi  import get_supplier_abdi
-
-
+from suppliers.fetchers.pakhshabdi.get_supplier_abdi import get_supplier_abdi
 
 products = load_available_products()
 
 for item in products:
+    supplier = get_supplier_abdi()
+    offer = find_offer_by_url(supplier, item["url"])
 
-    suppler = get_supplier_abdi()
-    offer = find_offer_by_url(suppler , item["url"])
     if offer:
-     if offer.purchase_price == item["price"]:        
-       print("tagir nakardeh")
+        if offer.purchase_price == item["price"]:
+            print("taghir nakarde")
+        else:
+            update_offer(offer, item)
     else:
         create_product_from_url(item["url"])
