02e83086aecfeb8d962c147c5ef706777dca0e18
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-05 09:57:03 +0330

    Update logger.py

--- suppliers/fetchers/pakhshabdi/sync/logger.py
+++ suppliers/fetchers/pakhshabdi/sync/logger.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(
     )
 )
 
-LOG_DIR = os.path.join(BASE_DIR, "scrap_abdisite", "logs")
+LOG_DIR = os.path.join(BASE_DIR, "pakhshabdi", "logs")
 
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
