8133cc7a01c6e04d553fb7bed35689c163d98b2c
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-06 15:46:00 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
+import os
+import django
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "bazbia_shop.settings"
+)
+
+django.setup()
+
+
 from suppliers.fetchers.pakhshabdi.sync_product_abdi import  sync_products
 sync_products()
