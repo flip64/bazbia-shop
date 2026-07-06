591f62f12557327a1d10b48302233d04a9538ab0
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-06 19:05:22 +0330

    Update adaptor_abdi.py

--- suppliers/fetchers/pakhshabdi/adaptor_abdi.py
+++ suppliers/fetchers/pakhshabdi/adaptor_abdi.py
@@ -1,13 +1,20 @@
+from suppliers.fetchers.pakhshabdi.json_loader import load_json
+from products.services.productdata_from_json import product_from_json
+
 from suppliers.fetchers.pakhshabdi.json_loader import load_available_products
 from suppliers.fetchers.pakhshabdi.extractor import extract_product_data
 
+PRODUCTDATA_FILE = "productdata.json"
+
 
 def list_product():
-    raw_products = load_available_products()
+    raw_products = load_json(PRODUCTDATA_FILE)
+
     product_data_list = []
 
     for item in raw_products:
-        product_data = extract_product_data(item["url"])
+        product_data = product_from_json(item)
+    
 
         if product_data:
             product_data_list.append(product_data)
