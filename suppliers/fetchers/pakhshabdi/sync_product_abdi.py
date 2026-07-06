251dd3b897907a43ca45668b7da5a14fec573e75
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-06 14:58:13 +0330

    Create sync_product_abdi.py

---
+++ suppliers/fetchers/pakhshabdi/sync_product_abdi.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from services.loader_json import load_json
+from services.save_json import save_json
+
+from extractor import extract
+from services.product_to_json import product_to_json
+
+
+AVAILABLE_PRODUCTS_FILE = "data/available_products.json"
+PRODUCTDATA_FILE = "data/productdata.json"
+
+
+def sync_products():
+
+    available_products = load_json(AVAILABLE_PRODUCTS_FILE)
+    productdata = load_json(PRODUCTDATA_FILE)
+
+    # ایندکس محصولات بر اساس URL
+    product_index = {
+        item["supplier_url"]: item
+        for item in productdata
+    }
+
+    for available in available_products:
+
+        url = available["supplier_url"]
+
+        # محصول وجود دارد
+        if url in product_index:
+
+            product = product_index[url]
+
+            product["price"] = available["price"]
+            product["quantity"] = available.get("quantity", 0)
+
+        # محصول جدید
+        else:
+
+            product = extract(url)
+
+            if product is None:
+                continue
+
+            productdata.append(
+                product_to_json(product)
+            )
+
+    save_json(PRODUCTDATA_FILE, productdata)
+
+
+if __name__ == "__main__":
+    sync_products()
