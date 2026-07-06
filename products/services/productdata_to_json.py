ac271cb7d0a3dfc6ec65b37ab0208753a4b10f6b
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-06 14:41:48 +0330

    Create productdata_to_json.py

---
+++ products/services/productdata_to_json.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+def product_to_json(product):
+    """
+    تبدیل ProductData به dict قابل تبدیل به JSON
+    """
+
+    return {
+        "name": product.name,
+        "slug": product.slug,
+        "description": product.description,
+        "category": product.category,
+
+        "price": product.price,
+        "quantity": product.quantity,
+
+        "images": product.images,
+        "videos": product.videos,
+
+        "specifications": product.specifications,
+        "attributes": product.attributes,
+        "tags": product.tags,
+
+        "supplier": product.supplier,
+        "supplier_url": product.supplier_url,
+        "supplier_product_code": product.supplier_product_code,
+
+        "is_active": product.is_active,
+
+        "brand": product.brand,
+        "barcode": product.barcode,
+        "weight": product.weight,
+    }
