75faf37e3e5405b60297bf93d1d4fe1bcf2c9833
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-08 16:47:08 +0330

    Update daily_price_report.py

--- suppliers/management/commands/daily_price_report.py
+++ suppliers/management/commands/daily_price_report.py
@@ -1,3 +1,18 @@
+import os
+import sys
+import django
+
+BASE_DIR = "/home/bazbiair/bazbia"
+sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "bazbia_shop.settings"
+)
+
+django.setup()
+
+
 from django.core.management.base import BaseCommand
 
 from suppliers.services.reports.daily_price_report import DailyPriceReportService
