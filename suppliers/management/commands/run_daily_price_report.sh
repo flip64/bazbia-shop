41c20c6ef1a9dbf3f7819945493f9ff6e7175577
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-08 15:34:43 +0330

    Update run_daily_price_report.sh

--- suppliers/management/commands/run_daily_price_report.sh
+++ suppliers/management/commands/run_daily_price_report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /home/bazbiair/virtualenv/bazbia/3.10/bin/activate && cd /home/bazbiair/bazbia
-cd suppliers/management/commands
-python daily_price_report.py
+
+python suppliers/management/commands/daily_price_report.py
