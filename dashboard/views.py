8b01125531e147dd662bc3fb2aa9179dc7c5e0c7
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-11 19:28:49 +0330

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,14 @@
 # dashboard/views.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from suppliers.models import Supplier
 @login_required
 def dashboard(request):
+    suppliers = Supplier.objects.filter(is_active=True)
+
+    
     context = {
         "page_title": "داشبورد مدیریت",
+        "supplers":suppliers
     }
     return render(request, "dashboard/index.html", context)
