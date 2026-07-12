bb64d9c6cf8fd6acbb11389271320ab2494516bd
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-12 15:25:42 +0330

    Update settings.py

--- bazbia_shop/settings.py
+++ bazbia_shop/settings.py
@@ -203,21 +203,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-# تنظیمات منطقه زمانی 
-
-
-import os
-from django.utils import timezone
-
 
 LANGUAGE_CODE = "fa-ir"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tehran"
 
-USE_TZ = False
+USE_I18N = True
 
-# USE_I18N = True
+USE_TZ = True
+
+
+
+
+
+
+
+
 
 
 
