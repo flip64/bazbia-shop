055fb44aace591f0f89d574041cce5b3a44f44f0
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-16 22:11:01 +0330

    Update __init__.py

--- dashboard/views/products/__init__.py
+++ dashboard/views/products/__init__.py
@@ -1,3 +1,20 @@
-from .list import *
-from .detail import *
-from .edit import *
+# -*- coding: utf-8 -*-
+
+from .list import (
+    product_list,
+    product_price_manager,
+    product_profit_manager,
+)
+
+from .detail import product_detail
+
+from .edit import product_info_edit
+
+
+__all__ = [
+    "product_list",
+    "product_detail",
+    "product_info_edit",
+    "product_price_manager",
+    "product_profit_manager",
+]
