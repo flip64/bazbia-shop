f8b404c7a56d4dfc5300988fca126dbcdf6de45d
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-16 15:46:58 +0330

    Update list.py

--- dashboard/views/products/list.py
+++ dashboard/views/products/list.py
@@ -1,1 +1,53 @@
 
+def product_list(request):
+    """
+    نمایش لیست تمام محصولات.
+    """
+
+    context = {
+        "page_title": "همه محصولات",
+    }
+
+    return render(
+        request,
+        "dashboard/pages/product_list.html",
+        context,
+    )
+
+
+
+
+@login_required
+def product_profit_manager(request):
+    """
+    مدیریت گروهی درصد سود محصولات.
+    """
+
+    context = {
+        "page_title": "مدیریت درصد سود",
+    }
+
+    return render(
+        request,
+        "dashboard/pages/product_profit.html",
+        context,
+    )
+
+
+
+
+@login_required
+def product_price_manager(request):
+    """
+    مدیریت قیمت فروش محصولات.
+    """
+
+    context = {
+        "page_title": "مدیریت قیمت فروش",
+    }
+
+    return render(
+        request,
+        "dashboard/pages/product_price.html",
+        context,
+    )
