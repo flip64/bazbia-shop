bd5fc9059871e6a13e79ef423aad6c937bf2de7d
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-16 15:49:13 +0330

    Update edit.py

--- dashboard/views/products/edit.py
+++ dashboard/views/products/edit.py
@@ -1,1 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404, render
 
+
+
+
+
+@login_required
+def product_edit(request, pk):
+    """
+    ویرایش یک محصول.
+    """
+
+    context = {
+        "page_title": "ویرایش محصول",
+        "product_id": pk,
+    }
+
+    return render(
+        request,
+        "dashboard/pages/product_edit.html",
+        context,
+    )
