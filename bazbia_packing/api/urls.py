f14d85cb882269db35526e82f8098a13e14bb2a6
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-27 13:53:52 +0330

    Update urls.py

--- bazbia_packing/api/urls.py
+++ bazbia_packing/api/urls.py
@@ -10,15 +10,11 @@ app_name = "bazbia_packing"
 
 
 urlpatterns = [
-    # API عمومی برای شرکت‌ها و سرویس‌های خارجی
-    # فعلاً غیرفعال است.
     path(
         "external/calculate/",
         ExternalPackingAPIView.as_view(),
         name="external-packing-calculate",
     ),
-
-    # API داخلی فروشگاه بازبیا
     path(
         "checkout/cart-quote/",
         CartShippingQuoteAPIView.as_view(),
