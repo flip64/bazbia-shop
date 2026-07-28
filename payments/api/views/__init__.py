0281f55842efe4bad5456d644f8770ca799a62b5
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-28 17:03:02 +0330

    Update __init__.py

--- payments/api/views/__init__.py
+++ payments/api/views/__init__.py
@@ -1,15 +1,10 @@
+# payments/api/views/__init__.py
+
 from .payment_create_view import (
     CreatePaymentView,
 )
-from .payment_verify_view import (
-    VerifyPaymentView,
-)
-from .payment_detail_view import (
-    PaymentDetailView,
-)
+
 
 __all__ = [
     "CreatePaymentView",
-    "VerifyPaymentView",
-    "PaymentDetailView",
 ]
