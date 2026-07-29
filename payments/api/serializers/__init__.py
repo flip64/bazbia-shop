92dfb12cf57163ac88e4958054dfd22ae85f2059
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-29 12:27:54 +0330

    Update __init__.py

--- payments/api/serializers/__init__.py
+++ payments/api/serializers/__init__.py
@@ -1,15 +1,20 @@
 from .payment_create_serializer import (
     CreatePaymentSerializer,
 )
-from .payment_verify_serializer import (
-    VerifyPaymentSerializer,
-)
 from .payment_detail_serializer import (
     PaymentSerializer,
 )
+from .payment_verify_serializer import (
+    VerifyPaymentSerializer,
+)
+from .zarinpal_callback_serializer import (
+    ZarinpalCallbackSerializer,
+)
+
 
 __all__ = [
     "CreatePaymentSerializer",
-    "VerifyPaymentSerializer",
     "PaymentSerializer",
+    "VerifyPaymentSerializer",
+    "ZarinpalCallbackSerializer",
 ]
