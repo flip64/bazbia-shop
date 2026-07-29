218be3172618494bd35f82bc8d44abf4dfffaa1f
flip64 <65002468+flip64@users.noreply.github.com>
2026-07-29 12:45:48 +0330

    Update __init__.py

--- payments/api/views/__init__.py
+++ payments/api/views/__init__.py
@@ -1,11 +1,20 @@
 from .payment_create_view import (
     CreatePaymentView,
 )
+from .payment_detail_view import (
+    PaymentDetailView,
+)
 from .payment_verify_view import (
     VerifyPaymentView,
 )
+from .zarinpal_callback_view import (
+    ZarinpalCallbackView,
+)
+
 
 __all__ = [
     "CreatePaymentView",
+    "PaymentDetailView",
     "VerifyPaymentView",
+    "ZarinpalCallbackView",
 ]
