021c85cabfe12f86991aa374e95b173fb6bb2a59
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-01 19:36:51 +0330

    Update management_urls.py

---
+++ torob_integration/management_urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .management_views import torob_variant_management
+
+
+app_name = "torob_management"
+
+urlpatterns = [
+    path(
+        "variants/",
+        torob_variant_management,
+        name="variants",
+    ),
+]
