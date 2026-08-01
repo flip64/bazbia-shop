8545b7af61d1a7c7fbbabdcc24991223b6b5267c
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-01 15:06:42 +0330

    Create urls.py

---
+++ torob_integration/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import TorobProductsAPIView
+
+
+app_name = "torob_integration_api"
+
+urlpatterns = [
+    path(
+        "v3/products",
+        TorobProductsAPIView.as_view(),
+        name="products-v3",
+    ),
+]
