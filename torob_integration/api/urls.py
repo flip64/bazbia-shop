6a6e9580bb7ce05d722bf6405548714707572fcc
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-01 15:13:10 +0330

    Update urls.py

--- torob_integration/api/urls.py
+++ torob_integration/api/urls.py
@@ -6,9 +6,5 @@ from .views import TorobProductsAPIView
 app_name = "torob_integration_api"
 
 urlpatterns = [
-    path(
-        "v3/products",
-        TorobProductsAPIView.as_view(),
-        name="products-v3",
-    ),
+    path("v3/products",TorobProductsAPIView.as_view(),name="products-v3"),
 ]
