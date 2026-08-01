8c7c0da2bce4415145bef7c52e44909657e465bd
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-01 12:28:38 +0330

    Update apps.py

--- torob_integration/apps.py
+++ torob_integration/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class TorobIntegrationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "torob_integration"
+    verbose_name = "اتصال به ترب"
+
+    def ready(self):
+        import torob_integration.signals  # noqa: F401
