40cfe5adb98b2cf5931ab7bb6ad9b80fe516acb3
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-01 23:08:24 +0330

    Update __init__.py

--- notifications/services/sms/__init__.py
+++ notifications/services/sms/__init__.py
@@ -1,1 +1,14 @@
 
+from .sms_ir import SMSIRService
+
+
+def send_otp_sms(
+    phone: str,
+    code: str,
+) -> dict:
+    service = SMSIRService()
+
+    return service.send_otp(
+        phone=phone,
+        code=code,
+    )
