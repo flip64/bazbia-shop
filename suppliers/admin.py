15499c7037dd24eb4a332516cd2543ef1fc3d77d
flip <jr64.naderloo@gmail.com>
2026-08-02 09:10:19 +0330

    edit admin

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from suppliers.models import Supplier
+from suppliers.models import Supplier,SupplierOffer
 
 
 
 admin.site.register(Supplier)
+admin.site.register(SupplierOffer)
 
