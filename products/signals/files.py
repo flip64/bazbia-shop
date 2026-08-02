c2b770a2e400eb66240c16bfabdaa7e72beb876d
flip <jr64.naderloo@gmail.com>
2026-08-02 08:47:17 +0330

    edit signals

--- products/signals/files.py
+++ products/signals/files.py
@@ -11,23 +11,32 @@ from products.models import (
 
 @receiver(post_delete, sender=Category)
 def delete_category_image(sender, instance, **kwargs):
+   try:
     if instance.image:
         instance.image.delete(save=False)
-
+   except: 
+    pass
 
 @receiver(post_delete, sender=ProductImage)
 def delete_product_image(sender, instance, **kwargs):
+   try: 
     if instance.image:
         instance.image.delete(save=False)
-
+   except: 
+      pass 
 
 @receiver(post_delete, sender=ProductVariantImage)
 def delete_variant_image(sender, instance, **kwargs):
+   try:
     if instance.image:
         instance.image.delete(save=False)
-
+   except : 
+      pass
 
 @receiver(post_delete, sender=ProductVideo)
 def delete_product_video(sender, instance, **kwargs):
+   try:  
     if instance.video:
         instance.video.delete(save=False)
+   except:
+     pass 
