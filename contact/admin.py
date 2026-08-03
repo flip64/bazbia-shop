9d36b38df8e969c9af0b4622a26449a1f290537c
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-03 15:18:38 +0330

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,96 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import ContactMessage
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "phone",
+        "subject_display",
+        "order_number",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "subject",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "phone",
+        "email",
+        "message",
+        "order_number",
+    )
+
+    readonly_fields = (
+        "user",
+        "name",
+        "phone",
+        "email",
+        "subject",
+        "order_number",
+        "message",
+        "ip_address",
+        "user_agent",
+        "created_at",
+        "updated_at",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+    list_per_page = 30
+
+    fieldsets = (
+        (
+            "اطلاعات پیام",
+            {
+                "fields": (
+                    "name",
+                    "phone",
+                    "email",
+                    "subject",
+                    "order_number",
+                    "message",
+                ),
+            },
+        ),
+        (
+            "مدیریت",
+            {
+                "fields": (
+                    "status",
+                    "admin_note",
+                ),
+            },
+        ),
+        (
+            "اطلاعات سیستمی",
+            {
+                "classes": (
+                    "collapse",
+                ),
+                "fields": (
+                    "user",
+                    "ip_address",
+                    "user_agent",
+                    "created_at",
+                    "updated_at",
+                ),
+            },
+        ),
+    )
+
+    @admin.display(
+        description="موضوع"
+    )
+    def subject_display(self, obj):
+        return obj.get_subject_display()
