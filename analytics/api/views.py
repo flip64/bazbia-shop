3d5e1d1bf075a4ea3a7d3e6bf2a0712fa964d2a1
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-08 23:38:07 +0330

    Update views.py

--- analytics/api/views.py
+++ analytics/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from analytics.models import SiteEvent
-from analytics.serializers import SiteEventCreateSerializer
+from analytics.api.serializers import SiteEventCreateSerializer
 
 
 def get_client_ip(request):
