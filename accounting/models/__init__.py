23d7b58b31a28f6d4df7f3008f022db66f1054c2
flip64 <65002468+flip64@users.noreply.github.com>
2026-08-10 15:51:42 +0330

    Update __init__.py

--- accounting/models/__init__.py
+++ accounting/models/__init__.py
@@ -2,6 +2,7 @@ from .account import Account
 from .fiscal_period import FiscalPeriod
 from .journal_entry import JournalEntry
 from .journal_item import JournalItem
+from .journal_sequence import JournalSequence
 
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     "FiscalPeriod",
     "JournalEntry",
     "JournalItem",
+    "JournalSequence",
 ]
