1ed43c61bb085379ac3f9362d7daba3e26a6d159
flip64 <jr64.naderloo@gmail.com>
2026-08-17 07:35:06 +0330

    delet scrapt abdi

--- scrap_abdisite/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('watched-urls/all/', views.watched_urls_all_api, name='watched_urls_all_api'),
-  # API برای بروزرسانی قیمت یک WatchedURL مشخص
-    path('watched-url/<int:pk>/update-price/', views.UpdateWatchedURLPriceAPIView.as_view(), name='update_watched_url_price_api'),
-    
-]
