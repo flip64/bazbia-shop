90d5e98c9947c3975e4bec13c0c740fa20d2c524
flip <jr64.naderloo@gmail.com>
2026-08-23 06:53:31 +0330

    migrate

--- customers/models.py
+++ customers/models.py
@@ -25,7 +25,6 @@ class CustomerLevel(models.Model):
 # مدل مشتری (Customer)
 # =============================
 
-
 class Customer(models.Model):
     user = models.OneToOneField(
         User,
@@ -55,9 +54,6 @@ class Customer(models.Model):
         default=0,
     )
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
     avatar = models.ImageField(
         upload_to="avatar/",
         help_text="مسیر ذخیره تصویر در media",
@@ -65,9 +61,17 @@ class Customer(models.Model):
         null=True,
     )
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.phone == "":
+            self.phone = None
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.phone or self.user.username
-
 
 # ==============================
 # مدل تضمین‌های مشتری (CustomerGuarantee)
@@ -144,8 +148,9 @@ class CustomerAddress(models.Model):
 # ==============================
 # مدل کد تایید  مشتری (OTP)
 # ==============================
-
 class OTP(models.Model):
+    MAX_ATTEMPTS = 5
+
     class Purpose(models.TextChoices):
         LOGIN = "login", "ورود"
         SET_PASSWORD = "set_password", "تنظیم رمز"
@@ -203,8 +208,36 @@ class OTP(models.Model):
         indexes = [
             models.Index(
                 fields=["phone", "purpose", "is_used"],
+                name="otp_phone_purpose_used_idx",
             ),
         ]
 
+    @property
+    def is_expired(self):
+        return timezone.now() >= self.expires_at
+
+    @property
+    def has_exceeded_attempts(self):
+        return self.attempts >= self.MAX_ATTEMPTS
+
+    @property
+    def is_valid(self):
+        return (
+            not self.is_used
+            and not self.is_expired
+            and not self.has_exceeded_attempts
+        )
+
+    def mark_as_used(self):
+        self.is_used = True
+        self.verified_at = timezone.now()
+
+        self.save(
+            update_fields=[
+                "is_used",
+                "verified_at",
+            ]
+        )
+
     def __str__(self):
-        return f"{self.phone} - {self.purpose}"
+        return f"{self.phone} - {self.get_purpose_display()}"
