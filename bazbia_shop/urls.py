97b1d2118e7c93bb57195008c87a57cf10646767
flip64 <65002468+flip64@users.noreply.github.com>
2026-09-04 20:19:10 +0330

    Update urls.py

--- bazbia_shop/urls.py
+++ bazbia_shop/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from products.sitemaps import ProductSitemap, CategorySitemap
+
+
 
 
 
@@ -49,7 +53,18 @@ urlpatterns = [
     path("api/analytics/", include("analytics.api.urls")),
     
     path("torob_api/",include("torob_integration.api.urls")),
+
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps},name="django.contrib.sitemaps.views.sitemap"),
+
+    
 ]
+
+
+sitemaps = {
+    "products": ProductSitemap,
+    "categories": CategorySitemap,
+}
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
