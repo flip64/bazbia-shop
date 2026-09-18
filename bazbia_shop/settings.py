aca866c0b4dea1044e9ce06a2fb038ef36efb116
flip64 <65002468+flip64@users.noreply.github.com>
2026-09-18 08:21:10 +0330

    Update settings.py

--- bazbia_shop/settings.py
+++ bazbia_shop/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from datetime import timedelta
 
+from decimal import Decimal
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
