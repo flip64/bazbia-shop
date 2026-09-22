77c772d7fa5b828297a55cb829f2f211d2c77dff
flip64 <65002468+flip64@users.noreply.github.com>
2026-09-22 06:41:13 +0330

    Update admin.py

--- basalam_integration/admin.py
+++ basalam_integration/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 
 from .models import (
     BasalamCategory,
+    BasalamCategoryAttributeSnapshot,
     BasalamCategoryMapping,
+    BasalamImageMapping,
     BasalamOrderMapping,
     BasalamProductMapping,
     BasalamVariationMapping,
