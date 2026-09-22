73ab5c599fb7bbf5c273c5f43604604b11f31c25
flip64 <65002468+flip64@users.noreply.github.com>
2026-09-22 06:35:21 +0330

    Create __init__.py

---
+++ basalam_integration/models/__init__.py
@@ -0,0 +1,30 @@
+from .category import BasalamCategory
+from .category_attribute import (
+    BasalamCategoryAttributeSnapshot,
+)
+from .category_mapping import (
+    BasalamCategoryMapping,
+)
+from .image_mapping import (
+    BasalamImageMapping,
+)
+from .order_mapping import (
+    BasalamOrderMapping,
+)
+from .product_mapping import (
+    BasalamProductMapping,
+)
+from .variation_mapping import (
+    BasalamVariationMapping,
+)
+
+
+__all__ = [
+    "BasalamCategory",
+    "BasalamCategoryAttributeSnapshot",
+    "BasalamCategoryMapping",
+    "BasalamImageMapping",
+    "BasalamOrderMapping",
+    "BasalamProductMapping",
+    "BasalamVariationMapping",
+]
